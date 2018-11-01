b095935f7ea127e8d91f1a2dbc09a00730a2ba23
tejus agarwal <tejusagarwal13@gmail.com>
2018-11-01 18:19:19 +0530

    Added Modifiers

--- contracts/OnlineQuiz.sol
+++ contracts/OnlineQuiz.sol
@@ -2,105 +2,107 @@ pragma solidity ^0.4.24;
     
     contract Combinatorial_Auction {
         
-        string question1;   //Storing the questions 1 to 4
-        string question2;
-        string question3;
-        string question4;
+        string Q1;   //Storing the questions 1 to 4
+        string Q2;
+        string Q3;
+        string Q4;
         
-        string answer1;     //Storing the answers 1 to 4
-        string answer2;
-        string answer3;
-        string answer4;
+        string A1;     //Storing the answers 1 to 4
+        string A2;
+        string A3;
+        string A4;
         
         uint n;             //max number of participants allowed in the quiz
-        uint totalFee;
-        uint participationFee;
-        uint quizMasterFee;
-        
-        address private quizMaster;
+        uint tFee;
+        uint pFee;
+        uint contractFee;
+        uint total;
+        address private Contractor;
         
         address[] private participants;
         
          mapping(address => uint) correctAnswerCountMapping;
          mapping(address => uint) public participantEarning;
         
-        //This makes sure that quizMaster cannot become a particiant
-        modifier notQuizMaster(){
-            require(msg.sender!=quizMaster,"QuizMaster cannot become a particiant");
+        //This makes sure that Contractor cannot become a particiant
+        modifier notContractor(){
+            require(msg.sender!=Contractor,"Contractor cannot become a particiant");
         _;}
         
-        //Everyone inputs the same participationFee
-        modifier feeEqualToPFee(uint _participationFee){
-            require(_participationFee==participationFee,"Any Fee other than participationFee is not acceptable");
+        //Everyone inputs the same pFee
+        modifier feeEqualToPFee(uint _pFee){
+            require(_pFee==pFee,"Any Fee other than pFee is not acceptable");
         _;}
         
         //Checks the number of participants in the game == n
-        modifier numberOfParticipants(){
-            require(n>0,"No more participants can participate");
+        modifier participantscount(){
+            require(total<=n,"Sorry, No other Participants can participate");
         _;}
-        
-        //Checks if the numberOfParticipants == n or we have reached revealDeadline
-        modifier verifyRevealDeadline(){
-            require(n==0,"More Participants are required");
+        modifier checkParticipantsN(){
+            require(total==n,"Game will start when all participants enter");
         _;}
-        
-        constructor(string _question1,string _answer1,string _question2,string _answer2,string _question3,string _answer3,string _question4,string _answer4,uint _participationFee, uint _n) 
+        modifier OnlyContractor(){
+            require(msg.sender==Contractor,"Only Contractor can calculate total amount to be paid!");
+        _;}
+        constructor(string _Q1,string _A1,string _Q2,string _A2,string _Q3,string _A3,string _Q4,string _A4,uint _pFee, uint _n) 
+        payable
         public
         {
-            question1 = _question1;
-            answer1 = _answer1;
+            Q1 = _Q1;
+            A1 = _A1;
             
-            question2 = _question2;
-            answer2 = _answer2;
+            Q2 = _Q2;
+            A2 = _A2;
             
-            question3 = _question3;
-            answer3 = _answer3;
+            Q3 = _Q3;
+            A3 = _A3;
             
-            question4 = _question4;
-            answer4 = _answer4;
+            Q4 = _Q4;
+            A4 = _A4;
             
-            participationFee = _participationFee;
+            pFee = _pFee;
             
             n = _n;
             
-            quizMaster = msg.sender;
+            Contractor = msg.sender;
             
-            totalFee = 0;
+            contractFee = n*pFee*1/4;
+            
+            tFee = 0;
+            total = 0;
         }
         
         function displayQuestion()
         public
-        view                                //https://ethereum.stackexchange.com/questions/27181/remix-warnings-state-mutability-and-public-visibility
+        view                                
         returns(string,string,string,string,uint)
         {
-            return(question1,question2,question3,question4,participationFee);
+            return(Q1,Q2,Q3,Q4,pFee);
         }
         
         
-        function registerParticipants(string _answer1,string _answer2,string _answer3,string _answer4)
+        function registerParticipants(string _A1,string _A2,string _A3,string _A4)
         public
-        payable
-        notQuizMaster()
-        feeEqualToPFee(msg.value)
-        numberOfParticipants()
+        notContractor()
+        participantscount()
         {
-            totalFee = totalFee + participationFee;
-            calculateCorrectAnswer(_answer1,_answer2,_answer3,_answer4);
-            n=n-1;
+            tFee = tFee + pFee;
+            calculateCorrectAnswer(_A1,_A2,_A3,_A4);
+            total=total+1;
         }
         
         
-        function calculateCorrectAnswer(string _answer1,string _answer2,string _answer3,string _answer4)
+        function calculateCorrectAnswer(string _A1,string _A2,string _A3,string _A4)
         private
         {
             uint count=0;
-            if(compareStrings(_answer1,answer1))
+            if(compareStrings(_A1,A1))
                 count++;
-            if(compareStrings(_answer2,answer2))
+            if(compareStrings(_A2,A2))
                 count++;
-            if(compareStrings(_answer3,answer3))
+            if(compareStrings(_A3,A3))
                 count++;
-            if(compareStrings(_answer4,answer4))
+            if(compareStrings(_A4,A4))
                 count++;
             
             correctAnswerCountMapping[msg.sender]=count;  
@@ -117,34 +119,37 @@ pragma solidity ^0.4.24;
             return keccak256(a) == keccak256(b);
         }
         
-        function calculateAmountPad()
+        function calculateAmountPaid()
         public
         view
-        verifyRevealDeadline()
+        OnlyContractor()
+        checkParticipantsN()
+
         {
             uint payableValue;
-            uint remainingFee=totalFee;
+            uint remainingFee=tFee;
             uint count;
             for(uint i=0;i<participants.length;i++){
                 payableValue=0;
                 count = correctAnswerCountMapping[participants[i]];
-                payableValue = (3*count*totalFee)/16;    
-                
+                payableValue = (3*count*tFee)/16;    
+                participantEarning[msg.sender]=payableValue;
                 remainingFee -= payableValue;
             }
-            totalFee = remainingFee;
+            tFee = remainingFee;
         }
-		function withdraw()
-    public
-    {
-        if(msg.sender == quizMaster){
-            msg.sender.transfer(quizMasterFee);
-            quizMasterFee = 0;
-        }
-        else
+        function withdraw()
+        payable
+        public
         {
-            msg.sender.transfer(participantEarning[msg.sender]);
-            participantEarning[msg.sender] = 0;
+            if(msg.sender == Contractor){
+                msg.sender.transfer(contractFee);
+                contractFee = 0;
+            }
+            else
+            {
+                msg.sender.transfer(participantEarning[msg.sender]);
+                participantEarning[msg.sender] = 0;
+            }
         }
-    }
     }
